56fad1ee9ee452ba6ac5b70c5ae0b66363583fb3
Csizmadia Márk <kindmysticaltree@gmail.com>
2024-11-29 13:37:09 +0100

    Add project files.

---
+++ SZGYA-WPF-AnimeList-20241129/Anime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_WPF_AnimeList_20241129
+{
+    internal class Anime
+    {
+    }
+}
